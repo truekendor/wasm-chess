d3e4acea63d46b70273c60226d9fc55fb9b18996
truekendor <sadqwerty000@gmail.com>
2026-05-14 22:13:18 +0300

    recovery from impossible castling tests

--- src/tests/castling_rights_tests.rs
+++ src/tests/castling_rights_tests.rs
@@ -266,6 +266,51 @@ pub mod castling_rights_tests {
         pretty_assertions::assert_eq!(castling_rights.queen, Some(true));
     }
 
+    #[test]
+    fn recovers_from_impossible_castling_set_attempt_white() {
+        // king on a second row
+        let fen = "2k4r/1q6/8/8/1P3P2/P3P1P1/3K4/R6R w - - 0 1".to_string();
+        let mut chess = WasmChess::new(Some(fen)).unwrap();
+
+        let ok = chess.set_castling_rights(
+            ColorChar::W,
+            CastlingObj {
+                king: Some(true),
+                queen: Some(true),
+            },
+        );
+
+        pretty_assertions::assert_eq!(ok, true);
+
+        let castling_rights = chess.get_castling_rights(ColorChar::W);
+
+        pretty_assertions::assert_eq!(castling_rights.king, Some(false));
+        pretty_assertions::assert_eq!(castling_rights.queen, Some(false));
+    }
+
+    #[test]
+    fn recovers_from_impossible_castling_set_attempt_black() {
+        // king moved (ok! might be a chess960 game),
+        // no rook on a queenside
+        let fen = "2k4r/1q6/8/8/1P3P2/P3P1P1/3K4/R6R b - - 0 1".to_string();
+        let mut chess = WasmChess::new(Some(fen)).unwrap();
+
+        let ok = chess.set_castling_rights(
+            ColorChar::B,
+            CastlingObj {
+                king: Some(true),
+                queen: Some(true),
+            },
+        );
+
+        pretty_assertions::assert_eq!(ok, true);
+
+        let castling_rights = chess.get_castling_rights(ColorChar::B);
+
+        pretty_assertions::assert_eq!(castling_rights.king, Some(true));
+        pretty_assertions::assert_eq!(castling_rights.queen, Some(false));
+    }
+
     // TODO:
     // add eventually ?
     // #[test]
