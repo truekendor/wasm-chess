1c5568c3eaf4d61cf42e9b1d040c96126763e6e8
truekendor <sadqwerty000@gmail.com>
2026-05-23 15:52:07 +0300

    change headers return type signature

--- src/tests/headers_tests.rs
+++ src/tests/headers_tests.rs
@@ -11,21 +11,19 @@ pub mod header_tests {
         chess.set_header("Event".to_string(), "Test Event".to_string());
         chess.set_header("Site".to_string(), "Test Site".to_string());
 
-        let answer = HeadersObj {
-            headers_data: OrderMap::from([
-                ("Event".to_string(), "Test Event".to_string()),
-                ("Site".to_string(), "Test Site".to_string()),
-                ("Date".to_string(), "????.??.??".to_string()),
-                ("Round".to_string(), "?".to_string()),
-                ("White".to_string(), "?".to_string()),
-                ("Black".to_string(), "?".to_string()),
-                ("Result".to_string(), "*".to_string()),
-            ]),
-        };
+        let answer = HeadersObj(OrderMap::from([
+            ("Event".to_string(), "Test Event".to_string()),
+            ("Site".to_string(), "Test Site".to_string()),
+            ("Date".to_string(), "????.??.??".to_string()),
+            ("Round".to_string(), "?".to_string()),
+            ("White".to_string(), "?".to_string()),
+            ("Black".to_string(), "?".to_string()),
+            ("Result".to_string(), "*".to_string()),
+        ]));
 
-        let headers = chess.get_headers().headers_data;
+        let headers = chess.get_headers().0;
 
-        pretty_assertions::assert_eq!(headers, answer.headers_data);
+        pretty_assertions::assert_eq!(headers, answer.0);
     }
 
     #[test]
@@ -37,21 +35,19 @@ pub mod header_tests {
         let removed = chess.remove_header("Event".to_string());
         assert!(removed);
 
-        let answer = HeadersObj {
-            headers_data: OrderMap::from([
-                ("Event".to_string(), "?".to_string()),
-                ("Site".to_string(), "Test Site".to_string()),
-                ("Date".to_string(), "????.??.??".to_string()),
-                ("Round".to_string(), "?".to_string()),
-                ("White".to_string(), "?".to_string()),
-                ("Black".to_string(), "?".to_string()),
-                ("Result".to_string(), "*".to_string()),
-            ]),
-        };
+        let answer = HeadersObj(OrderMap::from([
+            ("Event".to_string(), "?".to_string()),
+            ("Site".to_string(), "Test Site".to_string()),
+            ("Date".to_string(), "????.??.??".to_string()),
+            ("Round".to_string(), "?".to_string()),
+            ("White".to_string(), "?".to_string()),
+            ("Black".to_string(), "?".to_string()),
+            ("Result".to_string(), "*".to_string()),
+        ]));
 
-        let headers = chess.get_headers().headers_data;
+        let headers = chess.get_headers().0;
 
-        pretty_assertions::assert_eq!(headers, answer.headers_data);
+        pretty_assertions::assert_eq!(headers, answer.0);
 
         let non_existent = chess.remove_header("NonExistent".to_string());
 
@@ -67,22 +63,20 @@ pub mod header_tests {
         chess.set_header("Site".to_string(), "Test Site".to_string());
         chess.set_header("Opening".to_string(), "Santasiere's Folly".to_string());
 
-        let answer = HeadersObj {
-            headers_data: OrderMap::from([
-                ("Event".to_string(), "Test Event".to_string()),
-                ("Site".to_string(), "Test Site".to_string()),
-                ("Date".to_string(), "????.??.??".to_string()),
-                ("Round".to_string(), "?".to_string()),
-                ("White".to_string(), "?".to_string()),
-                ("Black".to_string(), "?".to_string()),
-                ("Result".to_string(), "*".to_string()),
-                ("Opening".to_string(), "Santasiere's Folly".to_string()),
-            ]),
-        };
+        let answer = HeadersObj(OrderMap::from([
+            ("Event".to_string(), "Test Event".to_string()),
+            ("Site".to_string(), "Test Site".to_string()),
+            ("Date".to_string(), "????.??.??".to_string()),
+            ("Round".to_string(), "?".to_string()),
+            ("White".to_string(), "?".to_string()),
+            ("Black".to_string(), "?".to_string()),
+            ("Result".to_string(), "*".to_string()),
+            ("Opening".to_string(), "Santasiere's Folly".to_string()),
+        ]));
 
-        let headers = chess.get_headers().headers_data;
+        let headers = chess.get_headers().0;
 
-        pretty_assertions::assert_eq!(headers, answer.headers_data);
+        pretty_assertions::assert_eq!(headers, answer.0);
     }
 
     #[test]
@@ -115,41 +109,39 @@ pub mod header_tests {
 
         chess.load_pgn(pgn).unwrap();
 
-        let answer = HeadersObj {
-            headers_data: OrderMap::from([
-                (
-                    "Event".to_string(),
-                    "CCC 25 Double-Fischer: Finals".to_string(),
-                ),
-                (
-                    "Site".to_string(),
-                    "https://www.chess.com/computer-chess-championship".to_string(),
-                ),
-                ("Date".to_string(), "2026.03.27".to_string()),
-                ("Round".to_string(), "19".to_string()),
-                ("White".to_string(), "Reckless".to_string()),
-                ("Black".to_string(), "Stockfish".to_string()),
-                ("Result".to_string(), "1/2-1/2".to_string()),
-                ("SetUp".to_string(), "1".to_string()),
-                (
-                    "FEN".to_string(),
-                    "nqbrkbrn/pppppp1p/6p1/8/7P/8/PPPPPPP1/BBNRNKRQ w GDgd - 0 2".to_string(),
-                ),
-                //
-                ("Custom1".to_string(), "Custom1".to_string()),
-                ("Custom2".to_string(), "Custom2".to_string()),
-                ("Custom3".to_string(), "Custom3".to_string()),
-                ("Custom4".to_string(), "Custom4".to_string()),
-                ("Custom5".to_string(), "Custom5".to_string()),
-                ("Custom6".to_string(), "Custom6".to_string()),
-                ("Custom7".to_string(), "Custom7".to_string()),
-                ("Custom8".to_string(), "Custom8".to_string()),
-                ("Custom9".to_string(), "Custom9".to_string()),
-            ]),
-        };
+        let answer = HeadersObj(OrderMap::from([
+            (
+                "Event".to_string(),
+                "CCC 25 Double-Fischer: Finals".to_string(),
+            ),
+            (
+                "Site".to_string(),
+                "https://www.chess.com/computer-chess-championship".to_string(),
+            ),
+            ("Date".to_string(), "2026.03.27".to_string()),
+            ("Round".to_string(), "19".to_string()),
+            ("White".to_string(), "Reckless".to_string()),
+            ("Black".to_string(), "Stockfish".to_string()),
+            ("Result".to_string(), "1/2-1/2".to_string()),
+            ("SetUp".to_string(), "1".to_string()),
+            (
+                "FEN".to_string(),
+                "nqbrkbrn/pppppp1p/6p1/8/7P/8/PPPPPPP1/BBNRNKRQ w GDgd - 0 2".to_string(),
+            ),
+            //
+            ("Custom1".to_string(), "Custom1".to_string()),
+            ("Custom2".to_string(), "Custom2".to_string()),
+            ("Custom3".to_string(), "Custom3".to_string()),
+            ("Custom4".to_string(), "Custom4".to_string()),
+            ("Custom5".to_string(), "Custom5".to_string()),
+            ("Custom6".to_string(), "Custom6".to_string()),
+            ("Custom7".to_string(), "Custom7".to_string()),
+            ("Custom8".to_string(), "Custom8".to_string()),
+            ("Custom9".to_string(), "Custom9".to_string()),
+        ]));
 
-        let headers = chess.get_headers().headers_data;
+        let headers = chess.get_headers().0;
 
-        pretty_assertions::assert_eq!(headers, answer.headers_data);
+        pretty_assertions::assert_eq!(headers, answer.0);
     }
 }
