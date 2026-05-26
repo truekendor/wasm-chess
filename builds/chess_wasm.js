90977bd6e88107723071e9e5538b48bf32f8c2aa
truekendor <sadqwerty000@gmail.com>
2026-05-26 17:54:35 +0300

    .......

--- builds/chess_wasm.js
+++
@@ -1,9 +0,0 @@
-/* @ts-self-types="./chess_wasm.d.ts" */
-
-import * as wasm from "./chess_wasm_bg.wasm";
-import { __wbg_set_wasm } from "./chess_wasm_bg.js";
-__wbg_set_wasm(wasm);
-wasm.__wbindgen_start();
-export {
-    WasmChess, findDivergence, movesToSan, movesToUci
-} from "./chess_wasm_bg.js";
